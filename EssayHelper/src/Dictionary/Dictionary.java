cfb7cb3756806818d8bbdb22fd4c48c050abd0e4
Eric-Cheang <eric.cheang.professional@gmail.com>
2014-06-05 21:34:40 -0500

    Began the great ifization

--- EssayHelper/src/Dictionary/Dictionary.java
+++ EssayHelper/src/Dictionary/Dictionary.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package Dictionary;
 import java.io.IOException;
@@ -28,7 +23,7 @@ public class Dictionary {
             Document definitionDoc = Jsoup.connect(
                     "http://www.oxforddictionaries.com/us/definition/american_english/" + toDefine
             ).followRedirects(true).get();
-            this.findInformationOxford(definitionDoc);
+            this.findInformationOxford(definitionDoc, toDefine);
             
     }
         catch(IOException e){
@@ -38,14 +33,60 @@ public class Dictionary {
     
 
     
-    private ArrayList findInformationOxford(Document definitionDoc){
+    private ArrayList findInformationOxford(Document definitionDoc, String word){
     
         String definition = definitionDoc.toString();
         definition = definition.substring(definition.indexOf("class=\"senseGroup\""));
-        
         ArrayList<DLinkedListNode> informationArray = new ArrayList();
-        
-        if ()
+        DLinkedListNode next = new DLinkedListNode("DUMMY", "DUMMY");
+        DLinkedListNode prev = next;
+        boolean whileBreaker = false;
+                
+        while(!whileBreaker){
+            if (definition.contains("class=\"partOfSpeech\"")){
+                definition = definition.substring(definition.indexOf("class=\"partOfSpeech\""));
+                DLinkedListNode newData = new DLinkedListNode(definition.substring(definition.indexOf(">") + 1, definition.indexOf("<")), "Word-partOfSpeech");
+                definition = definition.substring(definition.indexOf("class=\"partOfSpeech\"") + 20);   
+                prev.setNext(newData);
+                newData.setPrev(prev);
+                next = newData;
+                
+                
+                if (definition.contains("class=\"definition\"")){
+                    definition = definition.substring(definition.indexOf("class=\"definition\""));
+                    DLinkedListNode newData1 = new DLinkedListNode(definition.substring(definition.indexOf(">") + 1, definition.indexOf("<")), "Word-Definition");
+                    newData.setNext(newData1);
+                    newData1.setPrev(newData);
+                    definition = definition.substring(definition.indexOf("class=\"definition\"") + 18);
+                    
+                    if (definition.contains("class=\"example\"")){
+                        definition = definition.substring(definition.indexOf("class=\"example\""));
+                        DLinkedListNode newData2 = new DLinkedListNode(definition.substring(definition.indexOf(">")+1, definition.indexOf("<")), "Word-Example");
+                        newData1.setNext(newData2);
+                        newData2.setPrev(newData1);
+                        definition = definition.substring(definition.indexOf("class=\"example\"") + 15);
+                }
+                    else{
+                        DLinkedListNode newData2 = new DLinkedListNode("EMPTY", "Word-Example");
+                        newData1.setNext(newData2);
+                        newData2.setPrev(newData1);
+                    }   
+            }
+                else{
+                    DLinkedListNode newData1 = new DLinkedListNode("EMPTY", "Word-Definition");
+                    newData.setNext(newData1);
+                    newData1.setPrev(newData);
+                }
+        }
+            else{
+                if (definition.contains("class=\"definition\"")){
+                    definition = definition.substring(definition.indexOf("class=\"definition\""));
+                    DLinkedListNode newData3 = new DLinkedListNode(definition.substring(definition.indexOf(">") + 1, definition.indexOf("<")), "Phrase-Definition");
+                    newData3.setNext(newData1);
+                    newData1.setPrev(newData);
+                    definition = definition.substring(definition.indexOf("class=\"definition\"")
+                }
+            }
 
         System.out.println(definition);
         return informationArray;
@@ -54,103 +95,9 @@ public class Dictionary {
     
     
  /*
- * Main method is temporary as fuck
- * Excuse my french please
  * 
  */
     public static void main(String[] args){
         Dictionary test = new Dictionary("Sensible");
     }
 }
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package Dictionary;
-import java.io.IOException;
-import org.jsoup.*;
-import org.jsoup.examples.*;
-import org.jsoup.helper.*;
-import org.jsoup.nodes.*;
-import org.jsoup.parser.*;
-import org.jsoup.safety.*;
-import org.jsoup.select.*;
-import java.io.*;
-import java.util.*;
-/**
- *
- * @author Hobo
- */
-public class Dictionary {
-        
-
-    public Dictionary(String toDefine){
-        try{
-            Document definitionDoc = Jsoup.connect(
-                    "http://www.oxforddictionaries.com/us/definition/american_english/" + toDefine
-            ).followRedirects(true).get();
-            this.findInformationOxford(definitionDoc);
-            
-    }
-        catch(IOException e){
-            System.out.println("eror");
-        }
-    }
-    
-
-    
-    private ArrayList findInformationOxford(Document definitionDoc){
-    
-        String definition = definitionDoc.toString();
-        
-        int wordIndexCounter = 0;
-        int definitionIndexCounter = 0;
-        int SentenceIndexCounter = 0;
-        
-        ArrayList<String> informationArray = new ArrayList();
-
-        
-        definition = definition.substring(definition.indexOf("class=\"senseGroup\""));
-        while (definition.contains("class=\"iteration\"")){
-                            
-            if (definition.contains("class=\"partOfSpeech\"")){
-                definition = definition.substring(definition.indexOf("class=\"partOfSpeech\""));
-                informationArray.add(definition.substring(definition.indexOf(">") + 1, definition.indexOf("<")));
-            }
-            else{
-                definition = definition.substring(definition.indexOf("<h4>"));
-                informationArray.add(definition.substring(definition.indexOf(">") + 1, definition.indexOf("<")));
-            definition = definition.substring(definition.indexOf("class=\"definition\""));
-            informationArray.add(definition.substring(definition.indexOf(">") + 1,definition.indexOf(":")));
-            definition = definition.substring(definition.indexOf("class=\"example\""));
-            informationArray.add(definition.substring(definition.indexOf(">") + 1,definition.indexOf("<")));
-            }
-
-            
-            System.out.println("++++++++++++++++++++++++");
-            System.out.println(definition);
-            System.out.println(informationArray);
-            System.out.println("________________________");
-        }
-
-        //System.out.println(returnVariable[0]);
-        //System.out.println(returnVariable[1]);
-        
-        return informationArray;
-    }
-    
-    
-    
- /*
- * Main method is temporary as fuck
- * Excuse my french please
- * 
- */
-    public static void main(String[] args){
-        Dictionary test = new Dictionary("what");
-    }
-}
->>>>>>> 4ab359ac5c4a0b26bae44f2114add06f49c12c94
