95bc94f990eabd9630600722905d0b2129de2b06
Eric-Cheang <eric.cheang.professional@gmail.com>
2014-06-11 20:21:16 -0400

    Finished Dictionary class

--- EssayHelper/src/Dictionary/Dictionary.java
+++ EssayHelper/src/Dictionary/Dictionary.java
@@ -18,8 +18,8 @@ public class Dictionary {
         toDefine = toDefine.toLowerCase();
         toDefine = toDefine.replace(" ", "-");
         try{
-            System.setProperty("http.proxyHost", "149.89.1.30");
-            System.setProperty("http.proxyPort", "3128");
+            //System.setProperty("http.proxyHost", "149.89.1.30");
+            //System.setProperty("http.proxyPort", "3128");
             Document definitionDoc = Jsoup.connect(
                     "http://www.macmillandictionary.com/us/dictionary/american/" + toDefine
             ).followRedirects(true).get();
@@ -28,35 +28,30 @@ public class Dictionary {
             definition = definition.substring(definition.indexOf("<span class=\"DEFINITION\">"));
             definition = definition.substring(definition.indexOf("</span>"), definition.lastIndexOf("</span>") + 20);
             StringTokenizer tokenizer = new StringTokenizer(definition);
-            definition = "";
+            definition = " ******** \n ";
             
-            String tempToken = "";
             while (tokenizer.hasMoreTokens()){
                 String curToken = tokenizer.nextToken();
                 if (curToken.contains("title=") && curToken.contains(">")){
-                    definition = definition + curToken.substring(curToken.indexOf(">"), curToken.lastIndexOf(">")) + "";
-                    definition = definition.replaceAll("</a>", " ");
-                    definition = definition.replaceAll(">", "");
-                    definition = definition.replaceAll("</span", "");
-                    definition = definition.replaceAll("</a.", " ");
-                    definition = definition.trim();
-                    definition = definition.substring(0,1).toUpperCase() + definition.substring(1) + ".";
+                    if (curToken.contains("</span>")){
+                        curToken = curToken.substring(curToken.indexOf(">") + 1);
+                        curToken = curToken.substring(0,curToken.indexOf("<"));
+                        curToken = curToken + ". \n ******** \n";
+                    }
+                    else{
+                        curToken = curToken.substring(curToken.indexOf(">") + 1);
+                        curToken = curToken.substring(0,curToken.indexOf("<"));
+                    }
+                    definition = definition + curToken + " ";
                 }
             }
         }
         catch(Exception e){
             definition = "Definition not found.";
-            System.out.println(definition);
         }
     }
         
     public String getDefinition(){
         return definition;
-    }
-    
-
-    public static void main(String[] args){
-        Dictionary test = new Dictionary("home page");
-        System.out.println(test.getDefinition());
     }
 }
